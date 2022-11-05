e64f504a1c196a03a7fa03314c387e3030c4c8b3
Tristan <tristan.villarreal11@gmail.com>
2022-11-05 19:53:44 -0400

    first commit

---
+++ src/Week1Practice/Practice.java
@@ -0,0 +1,26 @@
+package Week1Practice;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		int myAge = 23;
+		String myName = "Tristan Villarreal";
+		char middleInitial = 'A';
+		
+		System.out.println("Name: "+ myName);
+		System.out.println("Middle Initial: "+ middleInitial);
+		System.out.println("Age: "+ myAge);
+		
+		if(myAge>18) {
+			
+			System.out.println("Your an adult");
+		}
+		else {
+			
+			System.out.println("Your a kid");
+		}
+
+	}
+
+}
